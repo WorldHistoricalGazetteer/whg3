5f706d57afdf0eb412dc14b042ca8cdf103f753b
Karl Grossner <karl.geog@gmail.com>
2023-08-05 13:45:57 +0200

    dscoll new

--- collection/views.py
+++ collection/views.py
@@ -861,7 +861,8 @@ class DatasetCollectionBrowseView(DetailView):
   redirect_field_name = 'redirect_to'
 
   model = Collection
-  template_name = 'collection/ds_collection_browse.html'
+  # template_name = 'collection/ds_collection_browse.html'
+  template_name = 'collection/ds_collection_browse_new.html'
 
   def get_success_url(self):
     id_ = self.kwargs.get("id")
