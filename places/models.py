cac02097082867bae6f2fb49834a7778c24cefbe
Karl Grossner <karl.geog@gmail.com>
2023-08-26 14:25:01 +0200

    fixture files created; migrations sorted

--- places/models.py
+++ places/models.py
@@ -195,7 +195,7 @@ class PlaceGeom(models.Model):
   def minmax(self):
     #tsarr=[]; intarr=[]
     #wg = self.jsonb['when']
-    from edtf import parse_edtf    
+    from edtf import parse_edtf
     def yearPadder(y):
       #print('y',y)
       year = str(y).zfill(5) if str(y)[0] == '-' else str(y).zfill(4)
@@ -284,18 +284,18 @@ class PlaceDepiction(models.Model):
     managed = True
     db_table = 'place_depiction'
 
-class CloseMatch(models.Model):
-  place_a = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="close_match1")
-  place_b = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="close_match2")
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
-  task = models.ForeignKey(TaskResult, on_delete=models.CASCADE, related_name="task",
-                           null=True, blank=True)
-  created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="matcher")
-  basis = models.CharField(max_length=200,
-      choices=[('authid', 'authority id'), ('reviewed', 'reviewed'), ('imported', 'imported')],
-      null=True, blank=True)
-
-  class Meta:
-    managed = True
-    db_table = 'close_matches'
+# class CloseMatch(models.Model):
+#   place_a = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="close_match1")
+#   place_b = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="close_match2")
+#   created_at = models.DateTimeField(auto_now_add=True)
+#   updated_at = models.DateTimeField(auto_now=True)
+#   task = models.ForeignKey(TaskResult, on_delete=models.CASCADE, related_name="task",
+#                            null=True, blank=True)
+#   created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="matcher")
+#   basis = models.CharField(max_length=200,
+#       choices=[('authid', 'authority id'), ('reviewed', 'reviewed'), ('imported', 'imported')],
+#       null=True, blank=True)
+#
+#   class Meta:
+#     managed = True
+#     db_table = 'close_matches'
