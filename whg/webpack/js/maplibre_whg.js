699fc16f827c733d02e3ed36e1be0a554185482a
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2023-09-17 12:24:43 +0100

    Popup now tracks mouse; fixed geo icons

--- whg/webpack/js/maplibre_whg.js
+++ whg/webpack/js/maplibre_whg.js
@@ -255,63 +255,37 @@ mappy.on('load', function() {
 
 	const layer_list = ['outline', 'gl_active_point', 'gl_active_line', 'gl_active_poly']
 
-	// popup generating events per layer
-	// TODO: polygons?
 	let activePopup;
 	for (var l in layer_list) {
-		mappy.on('mouseenter', layer_list[l], function(e) {
-			mappy.getCanvas().style.cursor = 'pointer';
-			
-			const ftype = e.features[0].layer.type
-			const gtype = e.features[0].geometry.type
-			const geom = e.features[0].geometry
-			const coords = e.features[0].geometry.coordinates
-			const place = e.features[0]
-			// console.log('geom, coords', geom, coords)
-			if (ftype == 'point') {
-				var coordinates = geom.coordinates.slice();
-			} else if (ftype == 'line') {
-				// could be simple linestring
-				if (gtype == 'LineString') {
-					const len = Math.round(geom.coordinates.length / 2)
-					var coordinates = geom.coordinates[len]
-				} else {
-					// MultiLineString
-					const segment = lineString(coords[Math.round(coords.length / 2)])
-					const len = length(segment)
-					var coordinates = along(segment, len / 2).geometry.coordinates
-				}
-			} else {
-				var coordinates = centroid(geom).geometry.coordinates
-			}
-			var pid = e.features[0].properties.pid;
-			var title = e.features[0].properties.title;
-			var src_id = e.features[0].properties.src_id;
-			var min = e.features[0].properties.min;
-			var max = e.features[0].properties.max;
-			var fc = e.features[0].properties.fclasses;
+	    mappy.on('mouseenter', layer_list[l], function(e) {
+	        mappy.getCanvas().style.cursor = 'pointer';
+	        
+	        var pid = e.features[0].properties.pid;
+	        var title = e.features[0].properties.title;
+	        var min = e.features[0].properties.min;
+	        var max = e.features[0].properties.max;
+	
+	        if (activePopup) {
+	            activePopup.remove();
+	        }
+	        activePopup = new maptilersdk.Popup({ closeButton: false })
+	            .setLngLat(e.lngLat)
+	            .setHTML('<b>' + title + '</b><br/>' +
+	                'Temporality: ' + (min ? min : '?') + '/' + (max ? max : '?') + '<br/>' +
+	                'Click to focus'
+	            )
+	            .addTo(mappy);
+	        activePopup.pid = pid;
+	    });
 
-			while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-				coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-			}
-			
-			activePopup = new maptilersdk.Popup({ closeButton: false })
-				.setLngLat(coordinates)
-				.setHTML('<b>' + title + '</b><br/>' +
-					// Cannot use href="javascript:<function>" in module. EventListener added to class instead.
-					// '<a href="#" class="fetch-info-link" data-pid="' + pid + '">fetch info</a><br/>' +
-					// 'start, end: ' + minmax +
-					'Temporality: ' + (min ? min : '?') + '/' + (max ? max : '?') + '<br/>' +
-					'Click to focus'
-					)
-				.addTo(mappy);
-			
+		mappy.on('mousemove', function(e) {
+		    if (activePopup) {
+		        activePopup.setLngLat(e.lngLat);
+		    }
 		});
 
-		// Change it back to a pointer when it leaves.
 		mappy.on('mouseleave', layer_list[l], function() {
 			mappy.getCanvas().style.cursor = '';
-			
 		    if (activePopup) {
 		      activePopup.remove();
 		    }
@@ -320,7 +294,11 @@ mappy.on('load', function() {
 
 		mappy.on('click', layer_list[l], function(e) {
 			
-			var pid = e.features[0].properties.pid;
+			var pid;
+			if (activePopup && activePopup.pid) {
+        		pid = activePopup.pid;
+		      	activePopup.remove();
+        	} else pid = e.features[0].properties.pid;
 			var table = $('#placetable').DataTable();
 		    
 		    // Search for the row within the sorted and filtered view
@@ -423,7 +401,6 @@ export const datasetLayers = [{
 				['boolean', ['feature-state', 'highlight'], false], 'red',
 				'#666'
 			],
-			// 'fill-outline-color': '#666'
 		},
 		'filter': ['==', '$type', 'Polygon']
 	},
