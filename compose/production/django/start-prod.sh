4aa468c1f1e6066c605bc45f2fa1589e6b9f317a
Karl Grossner <karl.geog@gmail.com>
2023-09-27 16:37:06 -0600

    config file touch-ups

---
+++ compose/production/django/start-prod.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+export POSTGRES_HOST_AUTH_METHOD="trust"
+
+python manage.py collectstatic --no-input
+
+# start the nginx server
+gunicorn whg.wsgi:application --bind 0.0.0.0:8003
