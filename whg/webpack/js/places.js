5003a8891a5639e22e7c31a3a99c5553a448c9f0
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2023-10-30 14:49:27 +0000

    Update places.js

--- whg/webpack/js/places.js
+++ whg/webpack/js/places.js
@@ -34,12 +34,6 @@ function waitMapLoad() {
     return new Promise((resolve) => {
         mappy.on('load', () => {
             console.log('Map loaded.');
-            const style = mappy.getStyle();
-            style.layers.forEach(layer => {
-                if (layer.id.includes('label')) {
-                    mappy.setLayoutProperty(layer.id, 'visibility', 'none');
-                }
-            });
             
             const pgeom = JSON.parse($('#pgeom script').text());
             const featureCollection = geomsGeoJSON([{geom: pgeom}]);
@@ -75,76 +69,52 @@ function waitDocumentReady() {
 
 Promise.all([waitMapLoad(), waitDocumentReady()])
     .then(() => {
+		//area_type = 'ccodes' // default
+		$(".textarea").each(function(index) {
+			if (["None", "null"].includes($(this).val())) {
+				$(this).val('')
+			}
+		});
+		$("#id_geojson").attr("placeholder", "generated from country codes")
+	
+		$(".a_more").click(function(e) {
+			clicked = $(this)
+			clicked.hide()
+			clicked.parent().find("#dots").hide()
+			clicked.next().removeClass('hidden')
+			//clicked.css("display","contents")
+			// $(this).hide()
+			console.log('clicked', clicked)
+			//clicked.removeClass('hidden')
+		})
+	
+		// temporal data from template json_script
+		window.pminmax = $('script').filter(function() {
+			return this.id == '{{ object.id }}' &&
+				this.text &&
+				this.text.includes('mm');
+		});
+		window.pts = $('script').filter(function() {
+			return this.id == '{{ object.id }}' &&
+				this.text &&
+				this.text.includes('ts');
+		});
+	
+		// don't try to make a histogram if no temporal data
+		if (pminmax.length > 0 && pts.length > 0) {
+			allts = JSON.parse(pts[0].text)['ts']
+			minmax = JSON.parse(pminmax[0].text)['mm']
+			console.log('allts', allts)
+			console.log('minmax', minmax)
+	
+			// feed to tvis_summary()
+			histogram_data(allts, minmax)
+		} else {
+			$("#place_temporal").hide()
+		}
  
     })
     .catch(error => console.error("An error occurred:", error));
-
-// leaflet map into modal
-function modalMap() {
-	mappo = L.map('mapdiv').setView([0, 0], 2);
-	var token_mb = '{{ mbtoken }}'
-
-	mbstyle_url = 'https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={token}';
-
-	osm = L.tileLayer(mbstyle_url, {
-		id: 'mapbox/light-v10',
-		token: token_mb
-	}).addTo(mappo)
-
-	geom = {
-		"type": "FeatureCollecton",
-		"features": []
-	}
-
-	// script includes geometry from calling portal page 
-	// filter to this pid
-	window.pgeoms = $('script').filter(function() {
-		return this.id == '{{ object.id }}' &&
-			this.text != '"null"' &&
-			this.text.includes('coordinates');
-	});
-
-	if (pgeoms.length > 0) {
-		for (i = 0; i < pgeoms.length; i++) {
-			let g = JSON.parse(pgeoms[i].text) 
-				//console.log(JSON.parse(pgeoms[i].text))
-				g['properties'] = {
-					"id": pgeoms[i].id
-				}
-			geom['features'].push(g)
-		}
-		layers = []
-		features = L.geoJSON(geom, {
-			onEachFeature: function(feature, layer) {
-				f = feature;
-				l = layer;
-				layers.push(layer)
-				//console.log('feature',f)
-				if (f.type != 'Point') {
-					console.log('f.type', f.type)
-					layer.setStyle(styles[f.type].default)
-				}
-			}
-		}).addTo(mappo);
-	}
-	zoomTo(layers[0])
-}
-
-//renderMap(geom)
-
-function zoomTo(lyr) {
-	//console.log('zoomTo()',lyr)
-	//l = idToFeature[pid]
-	ftype = lyr.feature.geometry.type
-	//console.log('zoomTo() pid, ftype',pid, ftype)
-	if (ftype == 'Point') {
-		mappo.setView(lyr._latlng, 7)
-	} else {
-		mappo.fitBounds(lyr.getBounds(), {
-			padding: [100, 100]
-		})
-	}
-}
 
 $("[rel='tooltip']").tooltip();
 
@@ -156,56 +126,6 @@ clip_geom.on('success', function(e) {
 		.tooltip('show');
 });
 // activate all tooltips
-
-$(function() {
-	//area_type = 'ccodes' // default
-	$(".textarea").each(function(index) {
-		if (["None", "null"].includes($(this).val())) {
-			$(this).val('')
-		}
-	});
-	$("#id_geojson").attr("placeholder", "generated from country codes")
-
-	$(".a_more").click(function(e) {
-		clicked = $(this)
-		clicked.hide()
-		clicked.parent().find("#dots").hide()
-		clicked.next().removeClass('hidden')
-		//clicked.css("display","contents")
-		// $(this).hide()
-		console.log('clicked', clicked)
-		//clicked.removeClass('hidden')
-	})
-
-	// temporal data from template json_script
-	window.pminmax = $('script').filter(function() {
-		return this.id == '{{ object.id }}' &&
-			this.text &&
-			this.text.includes('mm');
-	});
-	window.pts = $('script').filter(function() {
-		return this.id == '{{ object.id }}' &&
-			this.text &&
-			this.text.includes('ts');
-	});
-
-	// don't try to make a histogram if no temporal data
-	if (pminmax.length > 0 && pts.length > 0) {
-		allts = JSON.parse(pts[0].text)['ts']
-		minmax = JSON.parse(pminmax[0].text)['mm']
-		console.log('allts', allts)
-		console.log('minmax', minmax)
-
-		// feed to tvis_summary()
-		histogram_data(allts, minmax)
-	} else {
-		$("#place_temporal").hide()
-	}
-
-	// generate, populate map
-	//modalMap()
-
-}) // end onload()
 
 // helpers for histogram_data()
 function range(start, stop, step) {
@@ -376,11 +296,6 @@ $(".btn-cancel").click(function() {
 	$("#downloadModal").modal('hide')
 })
 
-function showMore() {
-	foo = $(this)
-	console.log('foo', foo)
-}
-
 $(".dl-links a").click(function(e) {
 	//e.preventDefault()
 	urly = '/datasets/' + $(this).data('id') + '/augmented/' + $(this).attr('ref')
@@ -393,147 +308,3 @@ $(".dl-links a").click(function(e) {
 		spinner_dl.stop();
 	})
 })
-
-// expose leaflet map for events, call it 'modalmap'
-window.addEventListener('modalmap:init', function(e) {
-	console.log('fired modalmap:init')
-	window.mapmodal = e.detail.map
-	mapmodal.setMaxBounds(null)
-
-
-	// style/tile urls
-	var mbstyle_url = 'https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={token}',
-		mbtiles_url = 'https://api.mapbox.com/v3/{id}/{z}/{x}/{y}.png',
-		ne_url = 'http://blog.whgazetteer.org/tiles/ne/{z}/{x}/{y}.png';
-
-	var satellite = L.tileLayer(mbstyle_url, {
-			id: 'mapbox/satellite-streets-v11',
-			token: token_mb
-		}),
-		osm = L.tileLayer(mbstyle_url, {
-			id: 'mapbox/light-v10',
-			token: token_mb
-		}),
-		ne_mb = L.tileLayer(mbtiles_url, {
-			id: 'kgeographer.ne_global',
-			token: token_whg
-		})
-
-	// GL wrappers to display heatmaps
-	mb_terrain = L.mapboxGL({
-		style: 'mapbox://styles/kgeographer/ckhf6o8yf07fw19qhbrm7f2q7',
-		accessToken: token_whg
-	})
-
-	ne_global = L.mapboxGL({
-		style: 'mapbox://styles/kgeographer/ckidglq8l2nra19nzzbl995ue',
-		accessToken: token_whg
-	}).addTo(mapmodal)
-
-
-	var baseLayers = {
-		"NE global": ne_global,
-		"Terrain": mb_terrain,
-		//"NE self": ne,
-		"OpenStreetMap": osm,
-		"Satellite": satellite
-	};
-
-	layerGroup = L.control.layers(baseLayers).addTo(mapmodal);
-
-}, false);
-
-
-//function zoomTo(id) {
-//mapmodal.setView(idToFeature[id]._latlng, mapmodal.getZoom() +2 )
-//}
-
-function cleanJson(pid, text) {
-	z = text.replace(/'/g, '\\"')
-	y = z.replace(/point/, 'Point')
-	geom = JSON.parse(y)
-	console.log('geom', geom)
-	return JSON.parse(geom)
-}
-/*
-styles = {
-	"MultiPolygon": {
-		"default": {
-			fillOpacity: 0.3,
-			opacity: 1,
-			color: "#000",
-			weight: 1,
-			fillColor: "#ff9999"
-		},
-		"focus": {
-			fillOpacity: 0.3,
-			opacity: 1,
-			color: "red",
-			weight: 2,
-			fillColor: "#ff9999"
-		}
-	},
-	"Polygon": {
-		"default": {
-			fillOpacity: 0.3,
-			opacity: .5,
-			color: "#666",
-			weight: 1,
-			fillColor: "#ff9999"
-		},
-		"focus": {
-			fillOpacity: 0.3,
-			opacity: .5,
-			color: "red",
-			weight: 2,
-			fillColor: "#ff9999"
-		}
-	}
-}
-*/
-// initialize, render map w/settings.LEAFLET_CONFIG
-function modalmap_init(map, options) {
-	//window.geom = {"type":"FeatureCollecton","features":[]}
-	window.mappo = map
-	console.log('in modalmap_init()', map)
-	window.geoms = []
-
-	window.gelems = $('script').filter(function() {
-		//return this.id.match(/[0-9]/) && this.text != '"null"';
-		console.log('this.id', this.id)
-		return this.id != '' && this.text != '"null"';
-	});
-	for (i = 0; i < gelems.length; i++) {
-		let t_geom = cleanJson(gelems[i].id, gelems[i].text)
-
-		//t_geom['properties'] = {"id": gelems[i].id,"ds": t_geom.ds!=null?t_geom.ds:ds}
-		geoms.push(t_geom)
-	}
-
-	if (geoms.length > 0) {
-		//console.log('geom: ',geom)
-		idToFeature = {} // for feature lookup
-		features = L.geoJSON(geoms, {
-			onEachFeature: function(feature, layer) {
-				f = feature;
-				l = layer;
-				//identifier = f.properties.id;
-				console.log('feature', f)
-				if (f.type != 'Point') {
-					layer.setStyle(styles[f.geometry.type].default)
-					//.bindPopup(f.properties.title+' ('+identifier+')'
-					//)
-					//idToFeature[identifier] = layer
-				}
-			}
-		}).addTo(map);
-
-		//mappy.setView(features.getBounds().getCenter(),6)
-		// fitBounds(bounds, {padding: [50, 50]})
-		mappy.options.maxZoom = 5
-		mappy.fitBounds(features.getBounds())
-		//mappy.setZoom(mappy.getZoom()-6)
-	} else {
-		console.log('no geometries, no feature')
-	}
-} // end map_init
