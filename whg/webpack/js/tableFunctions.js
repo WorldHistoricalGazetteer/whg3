cb3bc9f8a097bf4eb375cdf2cdb75c7552c144c3
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2023-12-06 16:25:17 +0000

    Dataset Browse table/map interaction debugged

--- whg/webpack/js/tableFunctions.js
+++ whg/webpack/js/tableFunctions.js
@@ -5,6 +5,7 @@ import { updatePadding, recenterMap/*, listSourcesAndLayers*/ } from './mapFunct
 import datasetLayers from './mapLayerStyles';
 import { mapSequencer } from './mapControls';
 import { mappy } from './mapAndTable';
+import { scrollToRowByProperty } from './tableFunctions-extended';
 
 let table;
 
@@ -97,38 +98,6 @@ function toggleMapLayers(mappy, val) {
 			}
 		});
 	});
-}
-
-export function scrollToRowByProperty(table, propertyName, value) {
-    // Search for the row within the sorted and filtered view
-    var pageInfo = table.page.info();
-    var rowPosition = -1;
-    var rows = table.rows({
-        search: 'applied',
-        order: 'current'
-    }).nodes();
-    let selectedRow;
-    for (var i = 0; i < rows.length; i++) {
-        var rowData = table.row(rows[i]).data();
-        rowPosition++;
-        if (rowData.properties[propertyName] == value) {
-            selectedRow = rows[i];
-            break; // Stop the loop when the row is found
-        }
-    }
-
-    if (rowPosition !== -1) {
-        // Calculate the page number based on the row's position
-        var pageNumber = Math.floor(rowPosition / pageInfo.length);
-
-        // Check if the row is on the current page
-        if (pageInfo.page !== pageNumber) {
-            table.page(pageNumber).draw('page');
-        }
-
-        selectedRow.scrollIntoView();
-        $(selectedRow).trigger('click');
-    }
 }
 
 export function highlightFeature(ds_pid, features, mappy) {
