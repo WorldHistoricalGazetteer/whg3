83df8276917b3b3d510af79e4d2fad7e1b229519
Karl Grossner <karl.geog@gmail.com>
2024-04-26 19:57:08 +0300

    workbench stubbed; modals need content

--- whg/webpack/js/workbench.js
+++ whg/webpack/js/workbench.js
@@ -1,22 +1,26 @@
 // Purpose: workbench-specific js
-  $(document).ready(function() {
+
+import '../css/workbench.css';
+
+$(document).ready(function() {
+  let page
   $(".help-matches").click(function() {
-    page = $(this).data('id')
-    console.log('help:', page)
-    $('.selector').dialog('open');
-  })
+      page = $(this).data('id')
+      console.log('help:', page)
+      $('.selector').dialog('open');
+    })
   $(".selector").dialog({
-  resizable: false,
-  autoOpen: false,
-  height: $(window).height() * 0.7,
-  width: $(window).width() * 0.6,
-  title: "WHG Help",
-  modal: true,
-  buttons: {'Close': function() {console.log('close dialog'); $(this).dialog('close');}},
-  open: function(event, ui) {
-  $('#helpme').load('/media/help/'+page+'.html')
-},
-  show: {effect: "fade",duration: 400},
-  hide: {effect: "fade",duration: 400}
-});
+      resizable: false,
+      autoOpen: false,
+      height: $(window).height() * 0.7,
+      width: $(window).width() * 0.6,
+      title: "WHG Workbench Pathways",
+      modal: true,
+      buttons: {'Close': function() {console.log('close dialog'); $(this).dialog('close');}},
+      open: function(event, ui) {
+      $('#helpme').load('/media/help/workbench/'+page+'.html')
+  },
+    show: {effect: "fade",duration: 400},
+    hide: {effect: "fade",duration: 400}
+  });
 })
