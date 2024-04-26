392394b5b545819fd8696cf1c171f567cfb7e242
Karl Grossner <karl.geog@gmail.com>
2024-04-26 09:58:29 +0300

    first login signal fixed

--- users/signals.py
+++ users/signals.py
@@ -9,11 +9,9 @@ User = get_user_model()
 
 @receiver(post_save, sender=User)
 def welcome_email(sender, instance, created, **kwargs):
-  """   Send welcome email to new user on registration w/reply-to editorial;
-        separate one to admins"""
-  # print('welcome_email signal received')
-  if instance.email_confirmed:
-    # print('welcome_email signal received, email confirmed')
+  """Send welcome email to new user on registration w/reply-to editorial;
+     separate one to admins"""
+  if created or instance.email_confirmed and not instance.__class__.objects.get(pk=instance.pk).email_confirmed:
     new_emailer(
       email_type='welcome',
       subject='Welcome to WHG',
@@ -33,3 +31,28 @@ def welcome_email(sender, instance, created, **kwargs):
       username=instance.username,
       id=instance.id
     )
+# def welcome_email(sender, instance, created, **kwargs):
+#   """   Send welcome email to new user on registration w/reply-to editorial;
+#         separate one to admins"""
+#   # print('welcome_email signal received')
+#   if instance.email_confirmed:
+#     # print('welcome_email signal received, email confirmed')
+#     new_emailer(
+#       email_type='welcome',
+#       subject='Welcome to WHG',
+#       from_email=settings.DEFAULT_FROM_EMAIL,
+#       reply_to=[settings.DEFAULT_FROM_EDITORIAL],
+#       to_email=[instance.email],
+#       greeting_name=instance.name if instance.name else instance.username,
+#       username=instance.username,
+#       name=instance.name
+#     )
+#     new_emailer(
+#       email_type='new_user',
+#       subject='New User Registered',
+#       from_email=settings.DEFAULT_FROM_EMAIL,
+#       to_email=settings.EMAIL_TO_ADMINS,
+#       name=instance.name,
+#       username=instance.username,
+#       id=instance.id
+#     )
