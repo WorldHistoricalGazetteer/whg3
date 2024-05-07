198978e0cac32c01df963e46e4eef4cf67d909cd
Karl Grossner <karl.geog@gmail.com>
2024-05-07 13:22:38 +0300

    all but two sections

--- utils/carousel_metadata.py
+++ utils/carousel_metadata.py
@@ -54,7 +54,7 @@ def carousel_metadata(caller):
         "image_file": caller.image_file.url if caller.image_file else None,
         "description": caller.description,
         "creator": caller.creator,
-        "contributors": caller.contributors or None,
+        "contributors": caller.contributors if caller_class == 'Dataset' else '',
         "owner": caller.owner.name,
         "type": caller_class.lower(), 
         "featured": caller.featured,
