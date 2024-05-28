ab9662a70a914377e0f2259d5a3b3879ba6854da
Karl Grossner <karl.geog@gmail.com>
2024-05-28 15:31:47 +0300

    stop tracking manaement files

--- places/models.py
+++ places/models.py
@@ -1,6 +1,7 @@
 # place.models
 from django.conf import settings
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
 from django.contrib.gis.db import models as geomodels
 from django.contrib.gis.db.models import Extent
@@ -13,11 +14,12 @@ from datasets.static.hashes.parents import ccodes as cc
 from main.choices import FEATURE_CLASSES, STATUS_REVIEW
 from django_celery_results.models import TaskResult
 from traces.models import TraceAnnotation
-# from edtf import parse_edtf
+
 
 def yearPadder(y):
   year = str(y).zfill(5) if str(y)[0] == '-' else str(y).zfill(4)
   return year
+
 
 class Place(models.Model):
   # id is auto-maintained, per Django
@@ -25,18 +27,18 @@ class Place(models.Model):
   src_id = models.CharField(max_length=2044, blank=True)
   # note FK is label, not id
   dataset = models.ForeignKey('datasets.Dataset', db_column='dataset',
-                                to_field='label', related_name='places', on_delete=models.CASCADE)
+                              to_field='label', related_name='places', on_delete=models.CASCADE)
   ccodes = ArrayField(models.CharField(max_length=2, null=True), blank=True)
   create_date = models.DateTimeField(null=True, blank=True)
-  minmax = ArrayField(models.IntegerField(blank=True, null=True),null=True,blank=True)
-  timespans = JSONField(blank=True,null=True) # for list of lists
+  minmax = ArrayField(models.IntegerField(blank=True, null=True), null=True, blank=True)
+  timespans = JSONField(blank=True, null=True)  # for list of lists
   fclasses = ArrayField(models.CharField(max_length=1, choices=FEATURE_CLASSES), null=True, blank=True)
-  indexed = models.BooleanField(default=False)  
+  indexed = models.BooleanField(default=False)
   idx_pub = models.BooleanField(default=False)
   idx_builder = models.BooleanField(default=False)
-  flag = models.BooleanField(default=False) # not in use
+  flag = models.BooleanField(default=False)  # not in use
   # added Apr 2023, for case of no start/end
-  attestation_year = models.IntegerField(null=True,blank=True)
+  attestation_year = models.IntegerField(null=True, blank=True)
 
   # 0=hits:unreviewed, 1=hits:reviewed, 2=hits:deferred, null=no hits
   review_wd = models.IntegerField(null=True, choices=STATUS_REVIEW)
@@ -66,9 +68,9 @@ class Place(models.Model):
   def extent(self):
     geoms = self.geoms.all()
     if geoms:
-        extent = geoms.aggregate(extent=Extent('geom'))
-        if extent['extent']:
-            return extent['extent']
+      extent = geoms.aggregate(extent=Extent('geom'))
+      if extent['extent']:
+        return extent['extent']
     return None
 
   @property
@@ -81,22 +83,23 @@ class Place(models.Model):
 
   @property
   def hashits_wd(self):
-    return self.hit_set.filter(authority__in=['wd','wdlocal']).count() >0
+    return self.hit_set.filter(authority__in=['wd', 'wdlocal']).count() > 0
 
   @property
   def hashits_whg(self):
-    return self.hit_set.filter(authority__in=['whg','idx']).count()>0
+    return self.hit_set.filter(authority__in=['whg', 'idx']).count() > 0
 
   @property
   def hashits_tgn(self):
-    return self.hit_set.filter(authority='tgn').count()>0
+    return self.hit_set.filter(authority='tgn').count() > 0
 
   @property
   def matches(self):
     close_matches = CloseMatch.objects.filter(
-        Q(place_a=self) | Q(place_b=self)
+      Q(place_a=self) | Q(place_b=self)
     )
-    distinct_places = set([self] + [match.place_a for match in close_matches] + [match.place_b for match in close_matches])
+    distinct_places = set(
+      [self] + [match.place_a for match in close_matches] + [match.place_b for match in close_matches])
     # First in returned list is the Primary Place, determined by having the most links
     matches = sorted(distinct_places, key=lambda place: (place.links.count(), place.id), reverse=True)
     return matches
@@ -126,32 +129,33 @@ class Place(models.Model):
     managed = True
     db_table = 'places'
     indexes = [
-          models.Index(fields=['src_id', 'dataset']),
-        ]
+      models.Index(fields=['src_id', 'dataset']),
+    ]
 
 
 class Type(models.Model):
   aat_id = models.IntegerField(unique=True)
-  parent_id = models.IntegerField(null=True,blank=True)
+  parent_id = models.IntegerField(null=True, blank=True)
   term = models.CharField(max_length=100)
   term_full = models.CharField(max_length=100)
   note = models.TextField(max_length=3000)
-  fclass = models.CharField(max_length=1,null=True,blank=True)
+  fclass = models.CharField(max_length=1, null=True, blank=True)
 
   def __str__(self):
-    return str(self.aat_id) +':'+self.term
+    return str(self.aat_id) + ':' + self.term
 
   class Meta:
     managed = True
     db_table = 'types'
 
+
 # NB in LPF spec but seldom used
 class Source(models.Model):
   owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
   # TODO: force unique...turn into slug or integer
-  src_id = models.CharField(max_length=30, unique=True)    # contributor's id
+  src_id = models.CharField(max_length=30, unique=True)  # contributor's id
   uri = models.URLField(null=True, blank=True)
-  label = models.CharField(max_length=255)    # short, e.g. title, author
+  label = models.CharField(max_length=255)  # short, e.g. title, author
   citation = models.CharField(max_length=500, null=True, blank=True)
 
   def __str__(self):
@@ -161,11 +165,12 @@ class Source(models.Model):
     managed = True
     db_table = 'sources'
 
+
 class PlaceName(models.Model):
   # {toponym, lang, citation{}, when{}}
   place = models.ForeignKey(Place, related_name='names',
-                              default=-1, on_delete=models.CASCADE)
-  src_id = models.CharField(max_length=100,default='') # contributor's identifier
+                            default=-1, on_delete=models.CASCADE)
+  src_id = models.CharField(max_length=100, default='')  # contributor's identifier
   jsonb = JSONField(blank=True, null=True)
   task_id = models.CharField(max_length=100, blank=True, null=True)
 
@@ -182,34 +187,37 @@ class PlaceName(models.Model):
   class Meta:
     managed = True
     db_table = 'place_name'
+    unique_together = ('place', 'src_id', 'jsonb',)
+
 
 class PlaceType(models.Model):
-  place = models.ForeignKey(Place,related_name='types',
-                              default=-1, on_delete=models.CASCADE)
-  src_id = models.CharField(max_length=100,default='') # contributor's identifier
+  place = models.ForeignKey(Place, related_name='types',
+                            default=-1, on_delete=models.CASCADE)
+  src_id = models.CharField(max_length=100, default='')  # contributor's identifier
   jsonb = JSONField(blank=True, null=True)
-  # identifier, label, sourceLabels[], when{}
 
-  aat_id = models.IntegerField(null=True,blank=True) # Getty AAT identifier
-  fclass = models.CharField(max_length=1,choices=FEATURE_CLASSES) # geonames feature class
+  aat_id = models.IntegerField(null=True, blank=True)  # Getty AAT identifier
+  fclass = models.CharField(max_length=1, choices=FEATURE_CLASSES)  # geonames feature class
 
   def __str__(self):
-    return (self.jsonb['sourceLabel'] if 'sourceLabel' in self.jsonb else '') +\
-           ('; ' + self.jsonb['label'] if 'label' in self.jsonb else '')
+    return (self.jsonb['sourceLabel'] if 'sourceLabel' in self.jsonb else '') + \
+      ('; ' + self.jsonb['label'] if 'label' in self.jsonb else '')
 
   class Meta:
     managed = True
     db_table = 'place_type'
+    unique_together = ('place', 'src_id', 'jsonb',)
+
 
 class PlaceGeom(models.Model):
-  place = models.ForeignKey(Place,related_name='geoms',
-                              default=-1, on_delete=models.CASCADE)
-  src_id = models.CharField(max_length=100,default='') # contributor's identifier
+  place = models.ForeignKey(Place, related_name='geoms',
+                            default=-1, on_delete=models.CASCADE)
+  src_id = models.CharField(max_length=100, default='')  # contributor's identifier
   jsonb = JSONField(blank=True, null=True)
   task_id = models.CharField(max_length=100, blank=True, null=True)
   geom_src = models.ForeignKey(Source, null=True, db_column='geom_src',
-                                 to_field='src_id', on_delete=models.SET_NULL)
-  # TODO: 
+                               to_field='src_id', on_delete=models.SET_NULL)
+  # TODO:
   geom = geomodels.GeometryField(null=True, blank=True, srid=4326)
   s2 = ArrayField(models.CharField(max_length=255, null=True), null=True)
   # informs dataset last_update
@@ -229,39 +237,44 @@ class PlaceGeom(models.Model):
   # good to have, but not accessible in values_list queries
   @property
   def minmax(self):
-    #tsarr=[]; intarr=[]
-    #wg = self.jsonb['when']
+    # tsarr=[]; intarr=[]
+    # wg = self.jsonb['when']
     from edtf import parse_edtf
     def yearPadder(y):
-      #print('y',y)
+      # print('y',y)
       year = str(y).zfill(5) if str(y)[0] == '-' else str(y).zfill(4)
       return year if int(y) > -9999 else '-9999'
+
     def getInt(expr):
-      #print('expr',expr)
-      return int(parse_edtf( yearPadder(list(expr.values())[0])).get_year() )
-    
-    #when = pg.jsonb['when'] if 'when' in pg.jsonb else None
+      # print('expr',expr)
+      return int(parse_edtf(yearPadder(list(expr.values())[0])).get_year())
+
+    # when = pg.jsonb['when'] if 'when' in pg.jsonb else None
     when = self.jsonb['when'] if 'when' in self.jsonb else None
     tsarr = when['timespans'] if when and 'timespans' in when else None
-    years=[]; nullset=set([None]); intarr=[]
+    years = [];
+    nullset = set([None]);
+    intarr = []
     if when and tsarr:
-      #years=[];nullset=set([None]);intarr=[]
+      # years=[];nullset=set([None]);intarr=[]
       for ts in tsarr:
         start = getInt(ts['start'])
         end = getInt(ts['end']) if 'end' in ts else start
-        years += [start,end]
-        intarr.append([start,end])
-      years = list(set(years)-nullset)
-    return [min(years),max(years)] if len(years)>0 else None
+        years += [start, end]
+        intarr.append([start, end])
+      years = list(set(years) - nullset)
+    return [min(years), max(years)] if len(years) > 0 else None
 
   class Meta:
     managed = True
     db_table = 'place_geom'
+    # unique_together = ('place', 'src_id', 'jsonb',)
+
 
 class PlaceLink(models.Model):
-  place = models.ForeignKey(Place,related_name='links',
-                              default=-1, on_delete=models.CASCADE)
-  src_id = models.CharField(max_length=100,default='') # contributor's identifier
+  place = models.ForeignKey(Place, related_name='links',
+                            default=-1, on_delete=models.CASCADE)
+  src_id = models.CharField(max_length=100, default='')  # contributor's identifier
   jsonb = JSONField(blank=True, null=True)
   task_id = models.CharField(max_length=100, blank=True, null=True)
 
@@ -281,51 +294,59 @@ class PlaceLink(models.Model):
   class Meta:
     managed = True
     db_table = 'place_link'
+    unique_together = ('place', 'src_id', 'jsonb',)
+
 
 class PlaceWhen(models.Model):
-  place = models.ForeignKey(Place,related_name='whens',
-                              default=-1, on_delete=models.CASCADE)
-  src_id = models.CharField(max_length=100,default='') # contributor's identifier
+  place = models.ForeignKey(Place, related_name='whens',
+                            default=-1, on_delete=models.CASCADE)
+  src_id = models.CharField(max_length=100, default='')  # contributor's identifier
   jsonb = JSONField(blank=True, null=True)
-  minmax = ArrayField(models.IntegerField(blank=True,null=True), null=True)
+  minmax = ArrayField(models.IntegerField(blank=True, null=True), null=True)
 
   class Meta:
     managed = True
     db_table = 'place_when'
+    unique_together = ('place', 'src_id', 'jsonb', )
+
 
 class PlaceRelated(models.Model):
-  place = models.ForeignKey(Place,related_name='related',
-                              default=-1, on_delete=models.CASCADE)
-  src_id = models.CharField(max_length=100,default='') # contributor's identifier
+  place = models.ForeignKey(Place, related_name='related',
+                            default=-1, on_delete=models.CASCADE)
+  src_id = models.CharField(max_length=100, default='')  # contributor's identifier
   jsonb = JSONField(blank=True, null=True)
-  # relation_type, relation_to, label, when{}, citation{label,id}, certainty
 
   class Meta:
     managed = True
     db_table = 'place_related'
+    unique_together = ('place', 'src_id', 'jsonb',)
+
 
 class PlaceDescription(models.Model):
-  place = models.ForeignKey(Place,related_name='descriptions',
-                              default=-1, on_delete=models.CASCADE)
-  src_id = models.CharField(max_length=100,default='') # contributor's identifier
+  place = models.ForeignKey(Place, related_name='descriptions',
+                            default=-1, on_delete=models.CASCADE)
+  src_id = models.CharField(max_length=100, default='')  # contributor's identifier
   jsonb = JSONField(blank=True, null=True)
   task_id = models.CharField(max_length=100, blank=True, null=True)
-  # id, value, lang
 
   class Meta:
     managed = True
     db_table = 'place_description'
+    # unique_together = ('place', 'src_id',)
+    # TOO BIG? unique_together = ('place', 'src_id', 'jsonb')
+
 
 class PlaceDepiction(models.Model):
-  place = models.ForeignKey(Place,related_name='depictions',
-                              default=-1, on_delete=models.CASCADE)
-  src_id = models.CharField(max_length=100,default='') # contributor's identifier
+  place = models.ForeignKey(Place, related_name='depictions',
+                            default=-1, on_delete=models.CASCADE)
+  src_id = models.CharField(max_length=100, default='')  # contributor's identifier
   jsonb = JSONField(blank=True, null=True)
-  # id, title, license
 
   class Meta:
     managed = True
     db_table = 'place_depiction'
+    # unique_together = ('place', 'src_id', 'jsonb',)
+
 
 class CloseMatch(models.Model):
   place_a = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="close_match1")
@@ -336,8 +357,8 @@ class CloseMatch(models.Model):
                            null=True, blank=True)
   created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="matcher")
   basis = models.CharField(max_length=200,
-      choices=[('authid', 'authority id'), ('reviewed', 'reviewed'), ('imported', 'imported')],
-      null=True, blank=True)
+                           choices=[('authid', 'authority id'), ('reviewed', 'reviewed'), ('imported', 'imported')],
+                           null=True, blank=True)
 
   class Meta:
     managed = True
