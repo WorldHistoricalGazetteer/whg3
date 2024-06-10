762b60f7732200f6e39bd5cc3afcebed050509b0
Stephen Gadd <stephen@docuracy.co.uk>
2024-06-10 13:47:59 +0100

    Create clear_mapdata_cache.py

---
+++ main/management/commands/clear_mapdata_cache.py
@@ -0,0 +1,18 @@
+# main/management/commands/clear_mapdata_cache.py
+import os
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+class Command(BaseCommand):
+    help = 'Clears the cache folder'
+
+    def handle(self, *args, **options):
+        cache_folder = settings.CACHES['default']['LOCATION']
+        try:
+            for filename in os.listdir(cache_folder):
+                file_path = os.path.join(cache_folder, filename)
+                if os.path.isfile(file_path):
+                    os.unlink(file_path)
+            self.stdout.write(self.style.SUCCESS('Cache folder cleared successfully'))
+        except Exception as e:
+            self.stderr.write(self.style.ERROR(f'Failed to clear cache folder: {e}'))
