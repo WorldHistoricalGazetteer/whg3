88f50767098cc06a6c5cdf8cce1157c834b53ac8
Stephen Gadd <stephen@docuracy.co.uk>
2024-06-13 13:45:35 +0100

    Update tasks.py

--- main/tasks.py
+++ main/tasks.py
@@ -6,7 +6,7 @@ logger = get_task_logger(__name__)
 redis_client = redis.StrictRedis(host='redis', port=6379, db=0)  # Use the service name from Docker Compose for the host
 from redis.exceptions import WatchError
 
-from celery import shared_task # these are @task decorators
+from celery import shared_task
 from celery.result import AsyncResult
 #from celery_progress.backend import ProgressRecorder
 from django.conf import settings
@@ -75,48 +75,6 @@ def needs_tileset(category=None, id=None, pointcount_threshold=5000, geometrycou
     
     return total_coords > pointcount_threshold, total_coords, total_geometries, total_places
 
-def request_tileset(category, id, action):
-    print(f'request_tileset() task: {category}-{id}')
-
-    # Acquire distributed lock
-    acquired_lock = redis_client.set(f'request_tileset_lock', 'locked', nx=True, ex=300)
-    print(f'acquired_lock: {category}-{id}-{action}', acquired_lock)
-
-    if not acquired_lock:
-        print('Another instance of request_tileset() is already running. Queueing request.')
-        redis_client.set(f'{category}-{id}-{action}', 'queued', ex=3600)
-        # Enqueue the request if the lock is already acquired
-        with redis_client.pipeline() as pipe:
-            while True:
-                try:
-                    pipe.watch(f'request_tileset_lock')
-                    pipe.multi()
-                    pipe.rpush('tileset_queue', f'{category}-{id}-{action}')
-                    pipe.execute()
-                    break
-                except WatchError:
-                    continue
-        return {'success': False, 'message': 'Request queued due to lock.'}
-
-    try:
-        # Process the current request
-        task = process_tileset_request.delay(category, id, action)
-        
-        # Wait for the task to complete
-        AsyncResult(task.id).get() # Do not redefine and call `request_tileset` as a celery task because this line will break it
-        
-        # After processing the current request, check if there are pending requests in the queue
-        while True:
-            queued_request = redis_client.lpop('tileset_queue')
-            if not queued_request:
-                break
-            queued_category, queued_id, queued_action = queued_request.decode('utf-8').split('-')
-            print(f'Dequeuing and processing queued request: {queued_category}-{queued_id}-{queued_action}')
-            task = process_tileset_request.delay(queued_category, queued_id, queued_action)
-            AsyncResult(task.id).get() # Wait for the task to complete
-    finally:
-        redis_client.delete(f'request_tileset_lock')
-
 @shared_task()
 def process_tileset_request(category, id, action):
     redis_client.set(f'{category}-{id}-{action}', 'pending', ex=3600)
@@ -159,12 +117,55 @@ def process_tileset_request(category, id, action):
                 )
             msg = f'Tileset {"created" if action == "generate" else "deleted"} successfully for {category}-{id}.'
             print(msg)
+            
+            # After completing the current request, recursively call process_tileset_request to handle any further queued requests
+            process_next_tileset_request()
+            
             return {'success': True, 'message': msg}
         else:
             redis_client.set(f'{category}-{id}-{action}', 'failed', ex=3600)
             msg = f'Tileset {"creation" if action == "generate" else "deletion"} failed for {category}-{id}.'
             print(msg)
             return {'success': False, 'message': msg}
+
+def process_next_tileset_request():
+    # Dequeue the next tileset request from tileset_queue and process it
+    queued_request = redis_client.lpop('tileset_queue')
+    if queued_request:
+        queued_category, queued_id, queued_action = queued_request.decode('utf-8').split('-')
+        print(f'Dequeuing and processing queued request: {queued_category}-{queued_id}-{queued_action}')
+        process_tileset_request.delay(queued_category, queued_id, queued_action)
+
+@shared_task()
+def request_tileset(category, id, action):
+    print(f'request_tileset() task: {category}-{id}')
+
+    # Acquire distributed lock
+    acquired_lock = redis_client.set(f'request_tileset_lock', 'locked', nx=True, ex=300)
+    print(f'acquired_lock: {category}-{id}-{action}', acquired_lock)
+
+    if not acquired_lock:
+        print('Another instance of request_tileset() is already running. Queueing request.')
+        redis_client.set(f'{category}-{id}-{action}', 'queued', ex=3600)
+        # Enqueue the request if the lock is already acquired
+        with redis_client.pipeline() as pipe:
+            while True:
+                try:
+                    pipe.watch(f'request_tileset_lock')
+                    pipe.multi()
+                    pipe.rpush('tileset_queue', f'{category}-{id}-{action}')
+                    pipe.execute()
+                    break
+                except redis.WatchError:
+                    continue
+        return {'success': False, 'message': 'Request queued due to lock.'}
+
+    try:
+        # Process the current request
+        process_tileset_request.delay(category, id, action)
+        
+    finally:
+        redis_client.delete(f'request_tileset_lock')
     
 def send_tileset_request(category=None, id=None, action='generate'):
     
