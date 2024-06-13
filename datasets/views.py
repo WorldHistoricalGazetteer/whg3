6be4b245068798b0aca2c044853f5899f0e893c1
Stephen Gadd <stephen@docuracy.co.uk>
2024-06-13 08:15:06 +0100

    Fix place collection colours; fix null dataset file error

--- datasets/views.py
+++ datasets/views.py
@@ -2414,7 +2414,7 @@ class DatasetPublicView(DetailView):
 
     placeset = ds.places.all()
 
-    if file.file:
+    if file:
       context['current_file'] = file
       context['format'] = file.format
       context['numrows'] = file.numrows
@@ -2424,6 +2424,7 @@ class DatasetPublicView(DetailView):
         place_id__in=placeset, task_id__contains='-').count()
       context['geoms_added'] = PlaceGeom.objects.filter(
         place_id__in=placeset, task_id__contains='-').count()
+        
     return context
 
 
