d642de74f61e257fb96a8b52ee67cee7d067b8b9
Stephen Gadd <stephen@docuracy.co.uk>
2024-06-23 15:19:33 +0100

    SSH Tunnel to WHG Database
    Requires `.ssh` folder containing environment variables, tunnel configuration file, and `.pem` key.

--- compose/local/django/entrypoint.sh
+++ compose/local/django/entrypoint.sh
@@ -7,8 +7,10 @@ set -o pipefail
 # exits if any of your variables is not set
 set -o nounset
 
-postgres_ready() {
-python << END
+if [ "$DB_HOST" != "host.docker.internal" ]; then # Ignore following codeblock if using remote Postgres Database through SSH Tunnel
+
+	postgres_ready() {
+	python << END
 import sys
 import os
 import psycopg2
@@ -22,13 +24,14 @@ try:
 except psycopg2.OperationalError:
     sys.exit(-1)
 sys.exit(0)
-
 END
-}
-until postgres_ready; do
-  >&2 echo 'Waiting for PostgreSQL to become available...'
-  sleep 1
-done
->&2 echo 'PostgreSQL is available'
+	}
+	until postgres_ready; do
+	  >&2 echo 'Waiting for PostgreSQL to become available...'
+	  sleep 1
+	done
+	>&2 echo 'PostgreSQL is available'
+	
+fi
 
 exec "$@"
