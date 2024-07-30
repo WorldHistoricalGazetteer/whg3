fdbe1e2d536ea6edc9a5bfbffe6ded0c7c02fc46
Stephen Gadd <stephen@docuracy.co.uk>
2024-07-30 21:41:29 +0100

    startup fixes

--- compose/production/django/entrypoint.sh
+++ compose/production/django/entrypoint.sh
@@ -4,4 +4,18 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+# Collect static files
+echo "Collecting static files..."
 python manage.py collectstatic --no-input
+
+# Wait for the database to be ready
+echo "Waiting for the database to be ready..."
+until pg_isready -h db_beta -p 5432 -U postgres; do
+  echo "Database not ready yet. Sleeping..."
+  sleep 2
+done
+
+echo "Database is ready. Starting Gunicorn..."
+
+# Start Gunicorn server
+exec gunicorn whg.wsgi:application --bind 0.0.0.0:8003 --timeout 1200 -w 4
