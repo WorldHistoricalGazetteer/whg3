6d418fec1e43b9142711b40a14a982a155d949e6
Stephen Gadd <stephen@docuracy.co.uk>
2024-07-31 06:26:56 +0100

    Use environment variables in compose+entrypoint

--- compose/production/django/entrypoint.sh
+++ compose/production/django/entrypoint.sh
@@ -8,9 +8,14 @@ set -o nounset
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
+# Ensure required environment variables are set
+: "${DB_HOST:?Environment variable DB_HOST is not set}"
+: "${DB_PORT:?Environment variable DB_PORT is not set}"
+: "${APP_PORT:?Environment variable APP_PORT is not set}"
+
 # Wait for the database to be ready
 echo "Waiting for the database to be ready..."
-until pg_isready -h db_beta -p 5432 -U postgres; do
+until pg_isready -h "${DB_HOST}" -p "${DB_PORT}" -U postgres; do
   echo "Database not ready yet. Sleeping..."
   sleep 2
 done
@@ -18,4 +23,4 @@ done
 echo "Database is ready. Starting Gunicorn..."
 
 # Start Gunicorn server
-exec gunicorn whg.wsgi:application --bind 0.0.0.0:8003 --timeout 1200 -w 4
+exec gunicorn whg.wsgi:application --bind 0.0.0.0:${APP_PORT} --timeout 1200 -w 4
