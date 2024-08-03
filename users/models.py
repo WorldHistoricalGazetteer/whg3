6fa91be449e3c3f27382ad6e7b0599650ed650b3
Stephen Gadd <stephen@docuracy.co.uk>
2024-08-03 08:58:07 +0100

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.core.validators import RegexValidator
 from main.choices import USER_ROLE
 
 
