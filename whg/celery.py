a9b4773e0ce1a6597cf778327e9bf098d3807330
Stephen Gadd <stephen@docuracy.co.uk>
2024-08-03 14:55:10 +0100

    Modify settings

--- whg/celery.py
+++ whg/celery.py
@@ -1,21 +1,25 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "whg.settings")
 
 app = Celery('whg')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
-#app.conf.broker_url = 'redis://localhost:6379'
 app.conf.result_expires = None
 
 
 # override Beat default daily cleanup task
 app.conf.result_expires = None
 
-# Load task modules from all registered Django app configs.
+# Load task modules from all registered Django app configs
 app.autodiscover_tasks()
+
+# Ensure Celery uses Django's logging configuration
+from logging.config import dictConfig
+dictConfig(settings.LOGGING)
 
 @app.task(bind=True)
 def debug_task(self):
