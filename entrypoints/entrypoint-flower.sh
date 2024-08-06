1265976e8118a1b982455481768b9185372ab872
Stephen Gadd <stephen@docuracy.co.uk>
2024-08-06 20:47:20 +0100

    Update entrypoint-flower.sh

--- entrypoints/entrypoint-flower.sh
+++ entrypoints/entrypoint-flower.sh
@@ -8,6 +8,14 @@ set -o nounset
 : "${CELERY_BROKER_URL:?Environment variable CELERY_BROKER_URL is not set}"
 : "${FLOWER_BASIC_AUTH:?Environment variable FLOWER_BASIC_AUTH is not set}"
 
+# Ensure Redis is available
+echo "Waiting for Redis at ${REDIS_HOST}:${REDIS_PORT} to be ready..."
+until nc -z "${REDIS_HOST}" "${REDIS_PORT}"; do
+  echo "Redis not available yet" >&2
+  sleep 1
+done
+echo "Redis is available" >&2
+
 echo "Waiting for Celery workers to be available..."
 until celery -A whg inspect ping; do
   echo "Celery workers not available" >&2
@@ -17,5 +25,5 @@ done
 echo "Celery workers are available" >&2
 
 # Start Flower
-exec celery -A whg --broker="${CELERY_BROKER_URL}" flower --basic_auth="${FLOWER_BASIC_AUTH}"
+exec celery -A whg --broker="${CELERY_BROKER_URL}" flower #--basic_auth="${FLOWER_BASIC_AUTH}"
 
