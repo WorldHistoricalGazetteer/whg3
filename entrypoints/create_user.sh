e663fbcb4b6695da7461572b60c438c4a61d0977
Stephen Gadd <stephen@docuracy.co.uk>
2024-08-08 10:58:55 +0100

    Update create_user.sh

--- entrypoints/create_user.sh
+++ entrypoints/create_user.sh
@@ -4,6 +4,10 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+# Print current user and group memberships
+echo "Current user: $(whoami)"
+echo "Groups: $(groups)"
+
 # Ensure environment variables are set
 : "${USER_NAME:?Environment variable USER_NAME is not set}"
 : "${WHGADMIN_PASSWORD:?Environment variable WHGADMIN_PASSWORD is not set}"
