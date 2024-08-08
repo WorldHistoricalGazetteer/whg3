d2767e2720d202582a10767fde844cbd85732697
Stephen Gadd <stephen@docuracy.co.uk>
2024-08-08 13:28:04 +0100

    sudoers

--- entrypoints/user_pwd.sh
+++ entrypoints/user_pwd.sh
@@ -15,3 +15,13 @@ echo "Groups: $(groups)"
 # Set the user's password using the hashed password
 sudo usermod "$USER_NAME" --password "$(openssl passwd -1 "$WHGADMIN_PASSWORD")"
 echo "Password changed for $USER_NAME"
+
+# Remove the user's sudoers file
+SUDOERS_FILE="/etc/sudoers.d/$USER_NAME"
+if [ -f "$SUDOERS_FILE" ]; then
+    echo "Removing sudoers file: $SUDOERS_FILE"
+    sudo rm "$SUDOERS_FILE"
+    echo "Sudoers file removed"
+else
+    echo "No sudoers file found for $USER_NAME"
+fi
