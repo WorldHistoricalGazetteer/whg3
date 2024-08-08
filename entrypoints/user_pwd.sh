f1d0b5e030b503228bec765893fe4bdad261b1e8
Stephen Gadd <stephen@docuracy.co.uk>
2024-08-08 12:49:16 +0100

    Update user_pwd.sh

--- entrypoints/user_pwd.sh
+++ entrypoints/user_pwd.sh
@@ -19,13 +19,6 @@ echo "Password changed for $USER_NAME"
 sudo cat /etc/sudoers
 
 # Remove the user from passwordless sudoers
-echo "$(openssl passwd -1 "$WHGADMIN_PASSWORD")" | sudo -S sed -i "/$USER_NAME ALL=(ALL) NOPASSWD: ALL/d" /etc/sudoers
+echo "$WHGADMIN_PASSWORD" | sudo -S sed -i "/$USER_NAME ALL=(ALL) NOPASSWD: ALL/d" /etc/sudoers
 
-sudo cat /etc/sudoers
-
-# Verify that the user was removed from sudoers
-if ! sudo grep -q "$USER_NAME ALL=(ALL) NOPASSWD: ALL" /etc/sudoers; then
-    echo "$USER_NAME has been removed from passwordless sudoers"
-else
-    echo "Failed to remove $USER_NAME from passwordless sudoers"
-fi
+echo "$WHGADMIN_PASSWORD" | sudo -S cat /etc/sudoers
