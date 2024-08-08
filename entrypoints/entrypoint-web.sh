b2766e5d8e251a13415d4d7290d5da7104888f79
Stephen Gadd <stephen@docuracy.co.uk>
2024-08-08 14:44:50 +0100

    ports

--- entrypoints/entrypoint-web.sh
+++ entrypoints/entrypoint-web.sh
@@ -16,4 +16,4 @@ source /app/entrypoints/wait_for_db.sh
 wait_for_db
 
 echo "Starting Gunicorn server..."
-exec gunicorn whg.wsgi:application --bind 0.0.0.0:8000 --timeout 1200 -w 4
+exec gunicorn whg.wsgi:application --bind 0.0.0.0:8004 --timeout 1200 -w 4
