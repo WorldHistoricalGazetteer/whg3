4879bb41a41ccfc9fb53f2950ac0381cf9c0f848
Stephen Gadd <stephen@docuracy.co.uk>
2024-08-08 11:28:06 +0100

    Fix user

--- entrypoints/create_user.sh
+++ entrypoints/create_user.sh
@@ -12,23 +12,5 @@ echo "Groups: $(groups)"
 : "${USER_NAME:?Environment variable USER_NAME is not set}"
 : "${WHGADMIN_PASSWORD:?Environment variable WHGADMIN_PASSWORD is not set}"
 
-# Check if the group exists; if not, create it with same name as user
-if ! getent group "$USER_NAME" >/dev/null 2>&1; then
-    groupadd -g 1000 "$USER_NAME"
-fi
-
-# Check if the user exists; if not, create it
-if ! id -u "$USER_NAME" >/dev/null 2>&1; then
-    useradd -m -d "/home/$USER_NAME" -s "/bin/bash" -u 1000 -g 1000 "$USER_NAME"
-fi
-
-# Grant sudo privileges to the user
-if command -v sudo >/dev/null 2>&1; then
-    usermod -aG sudo "$USER_NAME"
-fi
-
 # Set the user's password
 echo "$USER_NAME:$WHGADMIN_PASSWORD" | chpasswd
-
-# Switch to USER_NAME
-su - "$USER_NAME" -c "echo 'Switched to $USER_NAME'"
