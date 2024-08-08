26f7f75c1adbae8f15d69ef1df68bdc8ed205547
Stephen Gadd <stephen@docuracy.co.uk>
2024-08-08 10:13:17 +0100

    Update create_user.sh

--- entrypoints/create_user.sh
+++ entrypoints/create_user.sh
@@ -9,19 +9,19 @@ set -o nounset
 : "${WHGADMIN_PASSWORD:?Environment variable WHGADMIN_PASSWORD is not set}"
 
 # Check if the group exists; if not, create it with same name as user
-if ! getent group $USER_NAME >/dev/null 2>&1; then
-    groupadd -g 1000 $USER_NAME
+if ! getent group "$USER_NAME" >/dev/null 2>&1; then
+    sudo groupadd -g 1000 "$USER_NAME"
 fi
 
 # Check if the user exists; if not, create it
-if ! id -u $USER_NAME >/dev/null 2>&1; then
-    useradd -m -d "/home/$USER_NAME" -s "/bin/bash" -u 1000 -g 1000 $USER_NAME
+if ! id -u "$USER_NAME" >/dev/null 2>&1; then
+    sudo useradd -m -d "/home/$USER_NAME" -s "/bin/bash" -u 1000 -g 1000 "$USER_NAME"
 fi
 
 # Set the user's password
-echo "$USER_NAME:$WHGADMIN_PASSWORD" | chpasswd
+echo "$USER_NAME:$WHGADMIN_PASSWORD" | sudo chpasswd
 
 # Grant sudo privileges to the user
 if command -v sudo >/dev/null 2>&1; then
-    usermod -aG sudo $USER_NAME
+    usermod -aG sudo "$USER_NAME"
 fi
