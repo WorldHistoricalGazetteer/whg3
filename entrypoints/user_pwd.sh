61d3be76cf2456203421ed9f143f47234dc867ab
Stephen Gadd <stephen@docuracy.co.uk>
2024-08-08 12:26:07 +0100

    User permissions

--- entrypoints/user_pwd.sh
+++ entrypoints/user_pwd.sh
@@ -13,7 +13,17 @@ echo "Groups: $(groups)"
 : "${WHGADMIN_PASSWORD:?Environment variable WHGADMIN_PASSWORD is not set}"
 
 # Set the user's password using the hashed password
-echo "change_me" | sudo -S usermod "$USER_NAME" --password "$(openssl passwd -1 "$WHGADMIN_PASSWORD")"
+sudo usermod "$USER_NAME" --password "$(openssl passwd -1 "$WHGADMIN_PASSWORD")"
 
 # Optionally, verify that the password has been set
 echo "Password changed for $USER_NAME"
+
+# Remove the user from passwordless sudoers
+sudo sed -i "/$USER_NAME ALL=(ALL) NOPASSWD: ALL/d" /etc/sudoers
+
+# Verify that the user was removed from sudoers
+if ! sudo grep -q "$USER_NAME ALL=(ALL) NOPASSWD: ALL" /etc/sudoers; then
+    echo "$USER_NAME has been removed from passwordless sudoers"
+else
+    echo "Failed to remove $USER_NAME from passwordless sudoers"
+fi
