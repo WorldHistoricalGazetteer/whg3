3fbc9916d099859f1f25e6694dc5938972f48aa3
Stephen Gadd <stephen@docuracy.co.uk>
2024-08-12 12:42:11 +0100

    Reinstate direct Slack message on registration

--- users/signals.py
+++ users/signals.py
@@ -8,6 +8,9 @@ from whgmail.messaging import WHGmail
 import requests
 import logging
 
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
 logger = logging.getLogger(__name__)
 User = get_user_model()
 
@@ -25,3 +28,27 @@ def welcome_email(sender, instance, created, **kwargs):
             'username': instance.username,
             'name': instance.name,
         })
+
+        try:
+            slack_message = (
+                f"*Subject:* New User Registered\n"
+                f"*Username:* {instance.username}\n"
+                f"*Name:* {instance.name}\n"
+                f"*User ID:* {instance.id}\n"
+                f"----------------------------------------"
+            )
+            client = WebClient(token=settings.SLACK_BOT_OAUTH)
+            response = client.chat_postMessage(
+                channel='site-notifications',
+                text=slack_message
+            )
+            
+            if response["ok"]:
+                logger.info("Message sent to Slack.")
+            else:
+                logger.error(f"Failed to send message to Slack: {response['error']}")
+        
+        except SlackApiError as e:
+            logger.error(f"Slack API error: {e.response['error']}")
+        except Exception as e:
+            logger.exception("Error occurred while sending Slack notification for new user registration")
