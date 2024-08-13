94d1d79ace337bd4f9ed762b6b8fa52f1e266b5a
Stephen Gadd <stephen@docuracy.co.uk>
2024-08-13 17:12:51 +0100

    Reverse web entrypoint edits

--- entrypoints/entrypoint-web.sh
+++ entrypoints/entrypoint-web.sh
@@ -7,20 +7,20 @@ set -o nounset
 # Ensure required environment variables are set
 : "${APP_PORT:?Environment variable APP_PORT is not set}"
 
+# Change user password
+/app/entrypoints/permitted/user_pwd.sh
+
 # Collect static files
 echo "Collecting static files..."
-sudo /py/bin/python manage.py collectstatic --no-input
+/py/bin/python manage.py collectstatic --no-input
 
 # Wait for the database to be ready
 source /app/entrypoints/wait_for_db.sh
 wait_for_db
 
-# Run migrations
-echo "Running migrations..."
-sudo /py/bin/python manage.py migrate
-
-# Change user password
-/app/entrypoints/permitted/user_pwd.sh
+## Run migrations
+#echo "Running migrations..."
+#sudo /py/bin/python manage.py migrate
 
 echo "Starting Gunicorn server..."
 exec gunicorn whg.wsgi:application --bind 0.0.0.0:${APP_PORT} --timeout 1200 -w 4
