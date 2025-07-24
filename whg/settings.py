bac3d3eb50ef4c3c98dd412418ace3410e41f502
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2025-07-24 16:39:25 +0200

    Home page accordion

--- whg/settings.py
+++ whg/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.gis',
+    'django.contrib.humanize',
     'django.contrib.messages',
     'django.contrib.sessions',
     'django.contrib.sites',
