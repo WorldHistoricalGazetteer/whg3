0d4ff3e79a9314a35ccd835d39e751174376b234
Stephen Gadd <stephen@docuracy.co.uk>
2025-08-23 10:17:21 +0100

    Teaching View acceleration

--- whg/settings.py
+++ whg/settings.py
@@ -20,6 +20,8 @@ except ImportError:
 
 ENV_CONTEXT = os.environ.get('ENV_CONTEXT', 'dev-whgazetteer-org')  # Default if ENV_CONTEXT is not set
 
+DATABASES['default']['CONN_MAX_AGE'] = 600  # 10 minutes
+
 if 'test' in sys.argv:
     CELERY_ALWAYS_EAGER = True
 else:
